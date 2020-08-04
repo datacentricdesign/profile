df1d8d53330c73e0f40ccb58a51f1bb9f6729c4d
Jacky Bourgeois <J.Bourgeois@tudelft.nl>
2020-08-04 13:19:54 +0200

    keep trying db connection

--- profile-api/index.ts
+++ profile-api/index.ts
@@ -12,34 +12,54 @@ import errorMiddleware from './person/middlewares/ErrorMiddleware';
 import { PersonRouter } from "./person/PersonRouter";
 import { AuthRouter } from "./auth/AuthRouter";
 
-// Connects to the Relational Database -> then starts the express
-createConnection(config.orm)
-    .then(async connection => {
-        // Create a new express application instance
-        const app = express();
 
-        // Call middleware
-        app.use(cors());
-        app.use(helmet());
-        app.use(session({
-            secret: 'keyboard cat',
-            resave: false,
-            saveUninitialized: true,
-            cookie: { secure: true }
-          }))
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: false }));
-        app.use(cookieParser());
+waitAndConnect(1000);
 
-        // Set all routes from routes folder
-        app.use("/persons", PersonRouter);
-        app.use("/auth", AuthRouter);
-        app.use(errorMiddleware)
-
-        // Start listening
-        app.listen(config.http.port, () => {
-            console.log("Server started on port "+ config.http.port +"!");
+function waitAndConnect(delayMs:number) {
+    // Connects to the Relational Database -> then starts the express
+    createConnection(config.orm)
+        .then(async connection => {
+            await connection.runMigrations();
+            startAPI()
+        })
+        // Could not connect wait and try again
+        .catch((error) => {
+            console.log(JSON.stringify(error));
+            console.log("Retrying to connect in " + delayMs + " ms.");
+            delay(delayMs).then(()=>{
+                waitAndConnect(delayMs*1.5);
+            })
         });
+}
 
-    })
-    .catch(error => console.log(error));
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
+function startAPI() {
+    // Create a new express application instance
+    const app = express();
+
+    // Call middleware
+    app.use(cors());
+    app.use(helmet());
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true }
+      }))
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
+
+    // Set all routes from routes folder
+    app.use("/persons", PersonRouter);
+    app.use("/auth", AuthRouter);
+    app.use(errorMiddleware)
+
+    // Start listening
+    app.listen(config.http.port, () => {
+        console.log("Server started on port "+ config.http.port +"!");
+    });
+}
